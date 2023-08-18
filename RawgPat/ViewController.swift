a9841f86875e6e7dc1ac14bb169f6664de119070
Patrick Sinaga <sinagapatrick610@gmail.com>
2023-08-19 00:22:59 +0700

    Initial Commit

---
+++ RawgPat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RawgPat
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 19/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
